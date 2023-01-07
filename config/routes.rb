dd57c0fad5c18be92e69853abbf568ba5a94e427
zahmis <meda829ratteru86@gmail.com>
2023-01-07 09:03:44 +0900

    create rails

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # https://guides.rubyonrails.org/routing.html の DSL に従ってアプリケーションルートを定義します。
+
+  # ルートパスのルートを定義する ("/")
+  # ルート "articles#index"
+
+  root "application#hello" 
+
+
+end
