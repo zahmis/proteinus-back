c9a6ee38ef0e23ae4343f846f5b7d904f7101a0e
zahmis <meda829ratteru86@gmail.com>
2023-01-16 10:31:30 +0900

    add rubocop

--- Gemfile
+++ Gemfile
@@ -62,6 +62,9 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  gem 'rubocop-rails'
+  gem 
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html の DSL に従ってアプリケーションルートを定義します。
 
   # ルートパスのルートを定義する ("/")
-  # ルート "articles#index"
 
-  root "application#hello" 
-
-  
+  root 'application#index'
+  resources :users, only: [:index]
 end
