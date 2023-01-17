b400adba734234f1006e2dddfa66239c5e9cf77b
zahmis <meda829ratteru86@gmail.com>
2023-01-17 12:26:10 +0900

    add Users using seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a main sample user.
+User.create!(name: 'wai', email: 'wai@gmail.com', isLike: false)
+User.create!(name: 'kezago', email: 'kez@gmail.com', isLike: false)
+User.create!(name: 'nabe', email: 'nabe@nabe.com', isLike: false)
+User.create!(name: 'tam', email: 'tam@tam.com', isLike: false)
+User.create!(name: '12zawa', email: 'ichi@ichi.com', isLike: false)
+
